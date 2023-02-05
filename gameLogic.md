81ce28e166939bd2d24108cae2caa9fcd22a0a38
Jeneil Henry <shenry.jeneil@gmail.com>
2023-02-04 19:21:36 -0500

    Added images, changed styling, began JS DOM event listener and imagesArray

---
+++ gameLogic.md
@@ -0,0 +1,52 @@
+**MATCH**
+
+**Game Logic**
+
+Match is a game of matching two cars of the same type. 
+The player clicks on the back of one card, card turns over and stays on the screen. 
+The user clicks another card and if that card is a match, the matched cards are moved from the screen and the score increases.
+If the cards do not match then both cards are flipped back over showing the back of the card. 
+Useer  selects two cards again and try to math them.
+Game is over once user clears the board
+prompt reset
+** Two player idea -> two boards, two scores. The user that completes game first, wins. **
+
+**RESOURCES**
+
+setAttribute - https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
+getGttribute - https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute
+enevt listener - https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+setInterval - https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+time
+math random() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+sort() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+CSS flexbox - https://developer.mozilla.org/en-US/docs/Glossary/Flexbox
+
+**Color Scheme**
+
+Name: Blond (darker cream)
+Hex: #F9EEBD
+
+Name: Cream
+Hex: #FEFCD0
+
+Name: Chinese Green
+Hex: #D0DB61
+
+Name: Sap Green
+Hex: #458728
+
+Name: X11 Dark Green
+Hex: #006400
+
+
+**Code Explanation**
+
+1. Set up environment in VS
+2. get 6 images 100pxX100px square to make an environment that is 300X400, card back image, blank img - I chose 15 images 3x5
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,2 +1,134 @@
 console.log('hello World')
+// DOM event listener 
+document.addEventListener('DOMContentLoaded', () =>{
+// array of images with a name and image src
+    const cardArray = [
+        {
+            name: 'green apples'
+            img: 'images/greenApples.jpeg'
+        },
+        {
+            name: 'green brotha'
+            img: 'images/greenBrotha.jpeg'
+        },
+        {
+            name: 'green eye'
+            img: 'images/greenEye.jpeg'
+        },
+        {
+            name: 'green heart'
+            img: 'images/greenHeart.jpeg'
+        },
+        {
+            name: 'green abstract art'
+            img: 'images/greenAbstract.jpeg'
+        },
+        {
+            name: 'green leaves'
+            img: 'images/greenLeaves.jpeg'
+        }, 
+        {
+            name: 'green lime'
+            img: 'images/greenLime.jpeg'
+        },
+        {
+            name: 'green avocados'
+            img: 'images/greenAvocado.webp'
+        },
+        {
+            name: 'green sista'
+            img: 'images/greenSista.png'
+        },
+        {
+            name: 'monstera - swiss cheese plant'
+            img: 'images/greenMona.jpeg'
+        },
+        {
+            name: 'green friends'
+            img: 'images/greenFriends.jpeg'
+        },
+        {
+            name: 'green swamp photo'
+            img: 'images/greenSwamp.jpeg'
+        },
+        {
+            name: 'green plant leaves'
+            img: 'images/greenVessels.jpeg'
+        },
+        {
+            name: 'mint green gaming controller'
+            img: 'images/greenController.png'
+        },
+        {
+            name: 'green color pencils assortment'
+            img: 'images/greenColorpencils.jpeg'
+        },
+        // 15
+        {
+            name: 'green apples'
+            img: 'images/greenApples.jpeg'
+        },
+        {
+            name: 'green brotha'
+            img: 'images/greenBrotha.jpeg'
+        },
+        {
+            name: 'green eye'
+            img: 'images/greenEye.jpeg'
+        },
+        {
+            name: 'green heart'
+            img: 'images/greenHeart.jpeg'
+        },
+        {
+            name: 'green abstract art'
+            img: 'images/greenAbstract.jpeg'
+        },
+        {
+            name: 'green leaves'
+            img: 'images/greenLeaves.jpeg'
+        }, 
+        {
+            name: 'green lime'
+            img: 'images/greenLime.jpeg'
+        },
+        {
+            name: 'green avocados'
+            img: 'images/greenAvocado.webp'
+        },
+        {
+            name: 'green sista'
+            img: 'images/greenSista.png'
+        },
+        {
+            name: 'monstera - swiss cheese plant'
+            img: 'images/greenMona.jpeg'
+        },
+        {
+            name: 'green friends'
+            img: 'images/greenFriends.jpeg'
+        },
+        {
+            name: 'green swamp photo'
+            img: 'images/greenSwamp.jpeg'
+        },
+        {
+            name: 'green plant leaves'
+            img: 'images/greenVessels.jpeg'
+        },
+        {
+            name: 'mint green gaming controller'
+            img: 'images/greenController.png'
+        },
+        {
+            name: 'green color pencils assortment'
+            img: 'images/greenColorpencils.jpeg'
+        }
+        // 30
+
+    ]
+
+})
+
+
 
