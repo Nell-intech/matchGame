973947d84eac1ca0c4ab7ee40683b4dfdb7c7f3e
Jeneil Henry <shenry.jeneil@gmail.com>
2023-02-05 11:03:28 -0500

    Added next game images and changed background. Also, edited the JS

--- script2.js
+++ script2.js
@@ -4,125 +4,125 @@ document.addEventListener('DOMContentLoaded', () => {
     // array of images with a name and image src
     const cardArray = [
         {
-            name: 'green apples',
-            img: 'images/greenApples.jpeg' // relative path to each image
+            name: 'lux img 1',
+            img: 'images/lux1.jpeg' // relative path to each image
         },
         {
-            name: 'green brotha',
-            img: 'images/greenBrotha.jpeg'
+            name: 'lux img 2',
+            img: 'images/lux2.jpeg'
         },
         {
-            name: 'green eye',
-            img: 'images/greenEye.jpeg'
+            name: 'lux img 3',
+            img: 'images/lux3.jpeg'
         },
         {
-            name: 'green heart',
-            img: 'images/greenHeart.jpeg'
+            name: 'lux img 4',
+            img: 'images/lux4.jpeg'
         },
         {
-            name: 'green abstract art',
-            img: 'images/greenAbstract.jpeg'
+            name: 'glux img 5',
+            img: 'images/lux5.webp'
         },
         {
-            name: 'green leaves',
-            img: 'images/greenLeaves.jpeg'
+            name: 'lux img 6',
+            img: 'images/lux6.jpeg'
         },
         {
-            name: 'green lime',
-            img: 'images/greenLime.jpeg'
+            name: 'lux img 7',
+            img: 'images/lux7.jpeg'
         },
         {
-            name: 'green avocados',
-            img: 'images/greenAvocado.webp'
+            name: 'lux img 8',
+            img: 'images/lux8.jpeg'
         },
         {
-            name: 'green sista',
-            img: 'images/greenSista.png'
+            name: 'lux img 9',
+            img: 'images/lux9.jpeg'
         },
         {
-            name: 'monstera - swiss cheese plant',
-            img: 'images/greenMona.jpeg'
+            name: 'lux img 10',
+            img: 'images/lux10.jpeg'
         },
         {
-            name: 'green friends',
-            img: 'images/greenFriends.jpeg'
+            name: 'lux img 11',
+            img: 'images/lux11.jpeg'
         },
         {
-            name: 'green swamp photo',
-            img: 'images/greenSwamp.jpeg'
+            name: 'lux img 12',
+            img: 'images/lux12.jpeg'
         },
         {
-            name: 'green plant leaves',
-            img: 'images/greenVessels.jpeg'
+            name: 'lux img 13',
+            img: 'images/lux13.jpeg'
         },
         {
-            name: 'mint green gaming controller',
-            img: 'images/greenController.png'
+            name: 'lux img 14',
+            img: 'images/lux14.webp'
         },
         {
-            name: 'green color pencils assortment',
-            img: 'images/greenColorpencils.jpeg'
+            name: 'lux img 15',
+            img: 'images/lux15.jpeg'
         },
         // 15
         {
-            name: 'green apples',
-            img: 'images/greenApples.jpeg'
+            name: 'lux img 1',
+            img: 'images/lux1.jpeg' // relative path to each image
         },
         {
-            name: 'green brotha',
-            img: 'images/greenBrotha.jpeg'
+            name: 'lux img 2',
+            img: 'images/lux2.jpeg'
         },
         {
-            name: 'green eye',
-            img: 'images/greenEye.jpeg'
+            name: 'lux img 3',
+            img: 'images/lux3.jpeg'
         },
         {
-            name: 'green heart',
-            img: 'images/greenHeart.jpeg'
+            name: 'lux img 4',
+            img: 'images/lux4.jpeg'
         },
         {
-            name: 'green abstract art',
-            img: 'images/greenAbstract.jpeg'
+            name: 'glux img 5',
+            img: 'images/lux5.webp'
         },
         {
-            name: 'green leaves',
-            img: 'images/greenLeaves.jpeg'
+            name: 'lux img 6',
+            img: 'images/lux6.jpeg'
         },
         {
-            name: 'green lime',
-            img: 'images/greenLime.jpeg'
+            name: 'lux img 7',
+            img: 'images/lux7.jpeg'
         },
         {
-            name: 'green avocados',
-            img: 'images/greenAvocado.webp'
+            name: 'lux img 8',
+            img: 'images/lux8.jpeg'
         },
         {
-            name: 'green sista',
-            img: 'images/greenSista.png'
+            name: 'lux img 9',
+            img: 'images/lux9.jpeg'
         },
         {
-            name: 'monstera - swiss cheese plant',
-            img: 'images/greenMona.jpeg'
+            name: 'lux img 10',
+            img: 'images/lux10.jpeg'
         },
         {
-            name: 'green friends',
-            img: 'images/greenFriends.jpeg'
+            name: 'lux img 11',
+            img: 'images/lux11.jpeg'
         },
         {
-            name: 'green swamp photo',
-            img: 'images/greenSwamp.jpeg'
+            name: 'lux img 12',
+            img: 'images/lux12.jpeg'
         },
         {
-            name: 'green plant leaves',
-            img: 'images/greenVessels.jpeg'
+            name: 'lux img 13',
+            img: 'images/lux13.jpeg'
         },
         {
-            name: 'mint green gaming controller',
-            img: 'images/greenController.png'
+            name: 'lux img 14',
+            img: 'images/lux14.webp'
         },
         {
-            name: 'green color pencils assortment',
-            img: 'images/greenColorpencils.jpeg'
+            name: 'lux img 15',
+            img: 'images/lux15.jpeg'
         }
         // 30
     ]
@@ -151,7 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             let card = document.createElement('img')
-            card.setAttribute('src','images/greenBGI.svg')
+            card.setAttribute('src', 'images/luxBackground.png')
             card.setAttribute('data-id', i) // attaches a data if to each card from 0-29
             card.addEventListener('click', flipCard)
             flex.appendChild(card)
@@ -175,28 +175,30 @@ document.addEventListener('DOMContentLoaded', () => {
             cards[optionTwoId].setAttribute('src', 'images/eafeea.png')
             cardsWon.push(cardsChosen)
         } else {
-            //if the cars are not a match turn them back over allowing player to chose them again
-            cards[optionOneId].setAttribute('src', 'images/greenBGI.svg')
-            cards[optionTwoId].setAttribute('src', 'images/greenBGI.svg')
+            //if the carsd are not a match turn them back over allowing player to chose them again
+            cards[optionOneId].setAttribute('src', 'images/luxBackground.png')
+            cards[optionTwoId].setAttribute('src', 'images/luxBackground.png')
             alert(' Sorry, try again')
-            const playAgain = document.createElement('h2')
-            playAgain.innerHTML="Next Level!";
-            const heading = document.querySelector('.heading')
-            heading.appendChild(playAgain)
-            playAgain.style.border = "solid black"
-            playAgain.addEventListener('click',()=>{
-                window.location.href="index2.html/";
-            })
         }
         // if either of the two happen (card matches or does not match, clear the card array and cards chosen ID array)
         cardsChosen = []
         cardsChosenId = []
         //displaay results in browser to record how many times we have stored somethign in our cardsWon Array (1pt per match)
-        resultDisplay.textContent = cardsWon.length
+        resultDisplay.textContent = cardsWon.length * 3
         //if cards won === the length of cards array/2 alert user of completion of game
         if (cardsWon.length === cardArray.length / 2) {
             resultDisplay.textContent = ' Congrats! you win'
-           
+            
+            // //next level 3
+            //             const playAgain = document.createElement('h2')
+            //             playAgain.innerHTML="Next Level!";
+            //             const heading = document.querySelector('.heading')
+            //             heading.appendChild(playAgain)
+            //             playAgain.style.border = "solid black"
+            //             playAgain.addEventListener('click',()=>{
+            //                 window.location.href="index2.html/";
+            //             })
+
         }
     }
 
@@ -219,7 +221,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     createBoard()
     //checkForMatch()
-   flipCard()
+    flipCard()
 
 })
 
